1b0816af090bf2e225ad7fc6c20cdbbf8d4a7780
tunglx2 <tunglx2@vng.com.vn>
2023-06-24 18:29:44 +0700

    init app

---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+declare module '*.module.scss';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
+
+declare const __DEV__: boolean;
+declare const __PLATFORM__: 'win32' | 'darwin' | 'web';
